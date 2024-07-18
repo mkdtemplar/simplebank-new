68286649299daf2e33a15392e42a565f0fc6f78c
Ivan Markovski <sagitariusim@gmail.com>
2024-07-18 15:13:48 +0200

    updates

--- db/migrations/000002_add_users.up.sql
+++ db/migrations/000002_add_users.up.sql
@@ -3,9 +3,10 @@ CREATE TABLE "users" (
   "hashed_password" varchar NOT NULL,
   "full_name" varchar NOT NULL,
   "email" varchar UNIQUE NOT NULL,
-  "password_changet_at" timestampz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
+  "password_changet_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 ALTER TABLE "accounts" ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
-CREATE UNIQUE INDEX ON "accounts" ("owner", "currency");
+-- CREATE UNIQUE INDEX ON "accounts" ("owner", "currency");
+ALTER TABLE "accounts" ADD CONSTRAINT "owner_currency_key" UNIQUE ("owner", "currency");
