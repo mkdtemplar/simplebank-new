aad96978de80032a5870ac0675ed6bd82b1e7e31
Ivan Markovski <sagitariusim@gmail.com>
2024-07-18 11:13:19 +0200

    updates

---
+++ api/transfer.go
@@ -0,0 +1,64 @@
+package api
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	db "github.com/mkdtemplar/simplebank-new/db/sqlc"
+	"net/http"
+)
+
+type transferRequest struct {
+	FromAccountId int64  `json:"from_account_id" binding:"required,min=1"`
+	ToAccountId   int64  `json:"to_account_id" binding:"required,min=1"`
+	Amount        int64  `json:"amount" binding:"required,gt=0"`
+	Currency      string `json:"currency" binding:"required,oneof=USD EUR GBP"`
+}
+
+func (s *Server) createTransfer(ctx *gin.Context) {
+	var req transferRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	if !s.validAccount(ctx, req.FromAccountId, req.Currency) {
+		return
+	}
+
+	if !s.validAccount(ctx, req.ToAccountId, req.Currency) {
+		return
+	}
+
+	arg := db.TransferTxParams{
+		FromAccountID: req.FromAccountId,
+		ToAccountID:   req.ToAccountId,
+		Amount:        req.Amount,
+	}
+	result, err := s.store.TransferTx(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, result)
+}
+
+func (s *Server) validAccount(ctx *gin.Context, accountID int64, currency string) bool {
+	account, err := s.store.GetAccount(ctx, accountID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return false
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return false
+	}
+
+	if account.Currency != currency {
+		err = fmt.Errorf("invalid currency account [%d] %s vs %s", account.ID, currency, account.Currency)
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return false
+	}
+	return true
+}
