5e567a98ac3dc17ba254c8a616217ee7959ecd8f
Ivan Markovski <sagitariusim@gmail.com>
2024-07-21 19:46:28 +0200

    jwt paseto

---
+++ token/jwt_maker.go
@@ -0,0 +1,77 @@
+package token
+
+import (
+	"errors"
+	"fmt"
+	"time"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+const minSecretKeySize = 32
+
+type JWTMaker struct {
+	secretKay string
+}
+
+func NewJWTPayloadClaims(payload *Payload) *JWTPayloadClaims {
+	return &JWTPayloadClaims{
+		Payload: *payload,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(payload.ExpiredAt),
+			IssuedAt:  jwt.NewNumericDate(payload.IssuedAt),
+			NotBefore: jwt.NewNumericDate(payload.IssuedAt),
+			Issuer:    "simplebank",
+			Subject:   payload.Username,
+			ID:        payload.ID.String(),
+			Audience:  []string{"clients"},
+		},
+	}
+}
+
+func NewJWTMaker(secretKey string) (Maker, error) {
+	if len(secretKey) < minSecretKeySize {
+		return nil, fmt.Errorf("invalid key size: must be at least %d characters", minSecretKeySize)
+	}
+
+	return &JWTMaker{secretKay: secretKey}, nil
+}
+
+// CreateToken implements Maker.
+func (j *JWTMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+    if err != nil {
+        return "", err
+    }
+ 
+    jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, NewJWTPayloadClaims(payload))
+    signedString, err := jwtToken.SignedString([]byte(j.secretKay))
+    return signedString, err
+}
+
+// VerifyToken implements Maker.
+func (j *JWTMaker) VerifyToken(token string) (*Payload, error) {
+	jwtClaims := &JWTPayloadClaims{}
+    jwtToken, err := jwt.ParseWithClaims(token, jwtClaims, func(t *jwt.Token) (interface{}, error) {
+        if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+            return nil, ErrInvalidToken
+        }
+        return []byte(j.secretKay), nil
+    })
+ 
+    if err != nil {
+        if errors.Is(err, jwt.ErrTokenExpired) {
+            return nil, ErrExpiredToken
+        } else if errors.Is(err, ErrInvalidToken) {
+            return nil, ErrInvalidToken
+        } else {
+            return nil, err
+        }
+    }
+ 
+    payloadClaims, ok := jwtToken.Claims.(*JWTPayloadClaims)
+    if !ok {
+        return nil, ErrInvalidToken
+    }
+ 
+    return &payloadClaims.Payload, nil
+}
---
+++ token/paseto_maker.go
@@ -0,0 +1,54 @@
+package token
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/aead/chacha20poly1305"
+	"github.com/o1egl/paseto"
+)
+
+type PasetoMaker struct {
+	paseto      *paseto.V2
+	symetricKey []byte
+}
+
+func NewPasetoMaker(symetricKey string) (Maker, error) {
+	if len(symetricKey) != chacha20poly1305.KeySize {
+		return nil, fmt.Errorf("invalid key size: must be exacly %d characters", chacha20poly1305.KeySize)
+	}
+
+	maker := &PasetoMaker{
+		paseto:      paseto.NewV2(),
+		symetricKey: []byte(symetricKey),
+	}
+
+	return maker, nil
+}
+
+// CreateToken implements Maker.
+func (p *PasetoMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+
+	return p.paseto.Encrypt(p.symetricKey, payload, nil)
+}
+
+// VerifyToken implements Maker.
+func (p *PasetoMaker) VerifyToken(token string) (*Payload, error) {
+	payload := &Payload{}
+
+	err := p.paseto.Decrypt(token, p.symetricKey, payload, nil)
+	if err != nil {
+		return nil, ErrInvalidToken
+	}
+
+	err = payload.Valid()
+	if err != nil {
+		return nil, err
+	}
+
+	return payload, nil
+}
