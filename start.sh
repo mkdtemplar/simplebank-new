d1edffec8eb0202d153311114614ce316bf941e5
Ivan Markovski <sagitariusim@gmail.com>
2024-07-24 13:23:14 +0200

    deploy aded, secrets

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
+
 set -e
 echo "run db migration"
-source /app/app.env
+. /app/app.env
 /app/migrate -path /app/migrations -database "$DB_SOURCE" -verbose up
 echo "start the app"
 exec "$@"
